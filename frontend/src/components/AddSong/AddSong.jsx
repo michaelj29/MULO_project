c356c3f7eb39bf71c869022da44ac808e8364623
Michael J <johnson_michael1329@yahoo.com>
2022-05-26 17:29:41 -0500

    Add songs to the database from the frontend on the Artist page via add song component.

---
+++ frontend/src/components/AddSong/AddSong.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Form, Row , Col, Button, Card} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const AddSong = ({postSong}) => {
+
+    const [stageName, setStageName] = useState('');
+    const [songTitle, setSongTitle] = useState('');
+    const [videoId, setVideoId] = useState('');
+    const [genre, setGenre] = useState('');
+    const [year, setYear] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        let newSong = {
+            stage_name : stageName,
+            song_title: songTitle,
+            video_id: videoId,
+            genre: genre,
+            year: year
+        }
+        postSong(newSong)
+    };
+
+    return ( 
+        <div> 
+            <div className='fixed-bottom'>
+                <Card body>
+                    <Form onSubmit={handleSubmit}>
+                        <Row>
+                            <Col>
+                            <Form.Control placeholder="Stage Name" onChange={(event) => setStageName(event.target.value)} value={stageName}/>
+                            </Col>
+                            <Col>
+                            <Form.Control placeholder="Song Title" onChange={(event) => setSongTitle(event.target.value)} value={songTitle}/>
+                            </Col>
+                            <Col>
+                            <Form.Control placeholder="Video Id from Youtube" onChange={(event) => setVideoId(event.target.value)} value={videoId}/>
+                            </Col>
+                            <Col>
+                            <Form.Control placeholder="Genre" onChange={(event) => setGenre(event.target.value)} value={genre}/>
+                            </Col>
+                            <Col>
+                            <Form.Control placeholder="2022" onChange={(event) => setYear(event.target.value)} value={year}/>
+                            </Col>
+                            <Col xs="auto" className="my-1">
+                                <Button type="submit">Submit</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Card>
+            </div>
+        </div>
+     );
+}
+ 
+export default AddSong;
--- frontend/src/pages/ArtistPage/ArtistPage.js
+++ frontend/src/pages/ArtistPage/ArtistPage.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import AddSong from '../../components/AddSong/AddSong';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import useAuth from "../../hooks/useAuth";
 
 
 const ArtistPage = () => {
 
+    const [user, token] = useAuth();
+
+    
+    async function postSong(newSong){
+    try {
+        let response = await axios.post('http://127.0.0.1:8000/api/mulo/', newSong, {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          });
+    } catch (err) {
+        console.log('Error in postSong function in ArtistPage.js file')
+        
+    }
+    };
+
     return ( 
         <div>
             <h3>Artist Page </h3>
+            <AddSong fixed="bottom" postSong={postSong} /> 
         </div>
      );
 }
