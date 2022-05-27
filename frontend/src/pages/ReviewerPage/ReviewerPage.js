75340a5ab9674f08a722eb37e36c44038ff09a6d
Michael J <johnson_michael1329@yahoo.com>
2022-05-27 16:52:40 -0500

    Add delete functionality to a review.

--- frontend/src/pages/ReviewerPage/ReviewerPage.js
+++ frontend/src/pages/ReviewerPage/ReviewerPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import useAuth from "../../hooks/useAuth";
@@ -9,9 +10,11 @@ import AddReview from '../../components/AddReview/AddReview';
 const ReviewerPage = () => {
 
     const [user, token] = useAuth();
-    const [songs, setSongs] = useState([])
+    const [songs, setSongs] = useState([]);
+    const [reviews, setReviews] = useState([]);
 
-    
+    const myReviews = reviews.filter(review => review.user_id === user.id);
+
     async function postReview(newReview){
     try {
         let response = await axios.post('http://127.0.0.1:8000/api/mulo/create/', newReview, {
@@ -24,6 +27,23 @@ const ReviewerPage = () => {
         
     }
     };
+
+    useEffect(() => {
+        const fetchReviews = async () => {
+          try {
+            let response = await axios.get("http://127.0.0.1:8000/api/mulo/all-reviews/", {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            });
+            setReviews(response.data);
+
+          } catch (error) {
+            console.log(error.response.data);
+          }
+        };
+        fetchReviews();
+      }, [token]);
 
     useEffect(() => {
         const fetchSongs = async () => {
@@ -40,19 +60,32 @@ const ReviewerPage = () => {
         };
         fetchSongs();
       }, [token]);
+
+      
+        const deleteReview = async (selectedReview) => {
+          try {
+            let response = await axios.delete(`http://127.0.0.1:8000/api/mulo/${selectedReview}/`, {
+              headers: {
+                Authorization: "Bearer " + token,
+              },
+            });
+          } catch (error) {
+            console.log(error.response.data);
+          }
+        };
     return ( 
         <div>
-            <h3>Reviewer Page </h3>
+            <h3>Reviewer Page  {user.id}</h3>
             {songs.map(song => {
         return (
           <div>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '40rem' }}>
                 <iframe 
                     id="ytplayer" 
                     title="MyPlayer"
                     type="text/html" 
-                    width="286" 
-                    height="180"
+                    width="638" 
+                    height="380"
                     src={`https://www.youtube.com/embed/${song.video_id}?autoplay=1&origin=http://example.com`}
                     frameBorder="0">
                 </iframe>
@@ -66,6 +99,29 @@ const ReviewerPage = () => {
           </div>
         );
       })}
+
+      <div>
+      {myReviews.map(reviews => {
+        return (
+          <div>
+            <Card style={{ width: '20rem' }}>
+                <Card.Body>
+                    <Card.Title>{user.username} - - - {user.id}</Card.Title>
+                    <Card.Text>
+                    {`Rating: ${reviews.rating}    ${reviews.id}  Favorite Lyric: ${reviews.favorite_lyric}`}
+                    </Card.Text>
+                    <Card.Text>
+                    {`${reviews.overview}`}
+                    </Card.Text>
+                    <Button variant="danger" onClick={() => {
+                  deleteReview(reviews.id);
+                  }}>DELETE</Button>
+                </Card.Body>
+            </Card> 
+          </div>
+        );
+      })}
+      </div>
             <AddReview fixed="bottom" postReview={postReview}/>
         </div>
      );
